b14ea58c9be6cc31e2a87b42ebeeeeb490273c4d
Steve Hughes <Steve.Hughes@LeadVenture.com>
2021-05-07 17:28:17 -0400

    updated notes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 app = document.getElementById('app');
+
+
+
+// seo-keywords - exmaple 1
+// used cars, used car, used cars traverse city, used cars for sale, used cars near me, traverse city car dealers, used cars traverse city mi, 
+// used car dealerships, used car websites, usedcars, used car search, affordable used cars, traverse city used cars, used car sites, traverse city car dealerships, 
+// used cars for sale in michigan, cars for sale traverse city, used cars michigan, traverse city dealerships, traverse city auto dealers, used car dealers, 
+// used cars in michigan, car dealerships near me, used car dealerships near me, car dealerships traverse city, used car dealership, used car dealer, 
+// used cars for sale traverse city, car dealerships traverse city mi, car dealerships, used cars in traverse city, used autos, traverse city used car dealers, 
+// used cars traverse city michigan, car dealers, car dealerships in traverse city, cars for sale, car dealers traverse city, used car sales near me, 
+// dealerships in traverse city, affordable cars, traverse city mi used cars, used car dealerships in traverse city
+
+
+//SEO-metadata
+
+// Used Car Factory is an automotive dealer that specializes in selling preowned vehicles, including SUVs, trucks, and cars. Over the past 21 years in the automotive world, 
+//we have built a track record of providing customers with  high-quality, clean and affordable vehicles.
+
+//We have built an excellent reputation over the years by helping customers find exactly what they are looking for.  We pride ourselves on being a customer-focused, trustworthy, 
+//and reputable automobile dealer.  When it comes to vehicles, we know that every customer desires to get a reliable vehicle with the right features, 
+//performance and creature comforts. Our priority is to satisfy the needs of our customers by offering a wide selection of vehicles from a variety of manufacturers 
+//to suit their needs.  We strive to only acquire good-conditioned vehicles and source them from locations all over the country.
+
+//Our knowledge, experience & relentless pursuit of providing exceptional customer service makes us best choice in Traverse City.
